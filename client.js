51df915c5b1c07288cb26ac030a66e871bbbae7d
kintsugi <frostdown@live.com>
2026-08-23 20:50:13 -0500

    Update client.js

--- client.js
+++ client.js
@@ -1,13 +1,24 @@
-TrelloPowerUp.initialize({
-    'card-back-section': function (t, options) {
-        return {
-            title: 'Checklist Date Ranges',
-            icon: './icon.png',
-            content: {
-                type: 'iframe',
-                url: t.signUrl('./checklist-dates.html'),
-                height: 300
-            }
-        };
+TrelloPowerUp.initialize(
+    {
+
+        'card-back-section': function (t, options) {
+
+            return {
+                title: 'Checklist Date Ranges',
+
+                content: {
+                    type: 'iframe',
+                    url: t.signUrl('./checklist-dates.html'),
+                    height: 300
+                }
+            };
+
+        }
+
+    },
+
+    {
+        appKey: '366dbe929f3398a21e20a250d3fa4c17',
+        appName: 'Checklist Date Ranges'
     }
-});
+);
